98522003412e0de75a32cbcfdb3d02033aef3680
lingnvnv <lz155@duke.edu>
2021-05-04 18:42:30 -0400

    problem1 solved

--- least_factorial.py
+++ least_factorial.py
@@ -1,3 +1,29 @@
 def least_factorial(n: int) -> int:
-    pass
+    '''Find the smallest factorial which is not less than n
 
+    Given an integer n (1 ≤ n ≤ 120), the function find the
+    minimal k such that k = m! (where m! =1*2*... *m) for
+    some integer m, where k≥n
+
+    Args:
+        n (int): a positive integer from 1 to 120
+
+    Returns:
+        int : the smallest factorial integer which is not less
+        than n
+
+    Raises:
+        ValueError: Raises an exception if input is not a positive
+        integer from 1 to 120
+    '''
+    if type(n) is not int or n < 1 or n > 120:
+        raise ValueError("input should be a postive integer from 1 to 120")
+    m, product = 1, 1
+    while product < n:
+        m += 1
+        product *= m
+    return product
+
+
+if __name__ == "__main__":
+    pass
--- test_least_factorial.py
+++ test_least_factorial.py
@@ -1,15 +1,20 @@
 import pytest
+from least_factorial import least_factorial
 
 
-@pytest.mark.parametrize("n, expected", [
-    (17, 24),
-    (5, 6),
-    (106, 120)
-])
-def test_least_factorial(n, expected):
-    from least_factorial import least_factorial
-    
-    result = least_factorial(n)
-    assert result == expected
+class TestAssrt():
+    @pytest.mark.parametrize("n, expected", [
+        (17, 24),
+        (5, 6),
+        (106, 120)
+    ])
+    def test_least_factorial(self, n, expected):
+        result = least_factorial(n)
+        assert result == expected
 
-
+    def test_exception_match(self):
+        with pytest.raises(ValueError) as excinfo:
+            least_factorial(0)
+        assert excinfo.type == ValueError
+        assert 'input should be a postive integer from 1 to 120' in str(
+            excinfo.value)
